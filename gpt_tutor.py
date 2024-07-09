ca482e5b7e03b687b549aa095977e0e0cae160b8
isoudine <idaher@wisc.edu>
2024-07-09 16:08:18 -0500

    back to current

--- gpt_tutor.py
+++ gpt_tutor.py
@@ -1,16 +1,20 @@
 import os
-import json
 import openai
 import sys
-from openai import OpenAI
 import database_handler as dbh
+from openai import OpenAI
+from quiz import run_quiz
+from flashcards import run_flashcards
 
+
+# Get the valid subjects, subtopics, and study methods from the database.
 VALID_SUBJECTS = dbh.get_subjects()
 VALID_SUBTOPICS = {subject: dbh.get_subtopics(subject)
                    for subject in VALID_SUBJECTS}
 VALID_STUDY_METHODS = dbh.get_study_methods()
 
 # Set environment variables for the API key.
+# TODO: Set the enviornment variable!!!
 MY_API_KEY = os.environ.get('OPENAI_KEY')
 openai.api_key = MY_API_KEY
 # Create an OpenAPI client using the key.
@@ -123,133 +127,6 @@ def run_explanation(subject, subtopic):
     return response
 
 
-def run_quiz(subject, subtopic):
-    if subject == 'Math':
-        print('Unfortunately, current generative models are unable to '
-              'accurately supply correct, mathematical answers. Please '
-              'try again later!')
-        return
-
-    response_format_string = """
-        {
-            "Quiz": [
-                {
-                    "Question": "Which of the following...
-                    A. Option...
-                    B. Option...
-                    C. Option...
-                    D. Option...",
-                    "Answer": "The Correct Answer is: "
-                },
-                {
-                    "Question": "What is...
-                    A. Option...
-                    B. Option...
-                    C. Option...
-                    D. Option...",
-                    "Answer": "The Correct Answer is: "
-                }
-                ]
-        }
-        """
-    user_string = (f"Generate a {subject} five-question multiple-choice quiz "
-                   f"with four options (A,B,C,D) focused on {subtopic}."
-                   f"The response should be packaged in a JSON file that "
-                   f"follows this format {response_format_string}. Supply "
-                   f"option choices (A, B, C, D) in the 'Question' field"
-                   f"of the JSON, as dictated by the provided format. Always "
-                   f"supply the options to the question in the 'Question' "
-                   f"field. Ensure all questions are truly multiple choice. "
-                   f"Supply the answer to each multiple choice question as "
-                   f"demonstrated in the provided format sample. Strictly"
-                   f" adhere to the Key names provided in the format "
-                   f"for the returned JSON dictionary.")
-
-    system_string = (f"You are a helpful study assistant for students that"
-                     f"provides quiz questions and answers for a given subject"
-                     f" and topic, responding with a JSON file adhering to "
-                     f"this format {response_format_string}.")
-
-    print("\nStarting Quiz...\n")
-
-    response = CLIENT.chat.completions.create(
-        model="gpt-3.5-turbo",
-        response_format={"type": "json_object"},
-        messages=[
-            {"role": "system", "content": system_string},
-            {"role": "user", "content": user_string},
-        ]
-    )
-
-    dictionary_quiz = json.loads(response.choices[0].message.content)
-    quiz_key = list(dictionary_quiz.keys())[0]
-    list_quiz = dictionary_quiz[quiz_key]
-
-    for question_num, quiz_question in enumerate(list_quiz, start=1):
-        question_key = list(quiz_question.keys())[0]
-        answer_key = list(quiz_question.keys())[1]
-
-        print(f"Question {question_num}: {quiz_question[question_key]}")
-        user_answer = input("\nEnter your response to see the"
-                            " correct answer, or 'STOP' to quit: ")
-
-        if user_answer == 'STOP':
-            return
-        print(quiz_question[answer_key], "\n")
-
-    print("Quiz completed!\n")
-
-
-def run_flashcards(subject, subtopic):
-    format_string = """
-    {
-        "flashcards": [
-            {
-                "Definition": "",
-                "Term": ""
-            },
-            {
-                "Definition": "",
-                "Term": ""
-            }
-            ]
-    }
-    """
-
-    user_field = (f"Package 15 flashcards about the subtopic {subtopic} "
-                  f"about the subject {subject} and package them as a "
-                  f"JSON file that follows the format {format_string}, "
-                  f"and keep the name of the term out of the Definition "
-                  f"field. Given a definition, I will be able to supply the "
-                  f"vocabulary word.")
-
-    print("\nStarting 15 Flashcards...\n")
-
-    response = CLIENT.chat.completions.create(
-        model="gpt-3.5-turbo",
-        response_format={"type": "json_object"},
-        messages=[
-            {"role": "system", "content": "You are a helpful study assistant \
-            for students designed to output JSON."},
-            {"role": "user", "content": user_field}
-        ]
-    )
-
-    dictionary_flashcards = json.loads(response.choices[0].message.content)
-    flashcard_key = list(dictionary_flashcards.keys())[0]
-    list_flashcards = dictionary_flashcards[flashcard_key]
-
-    for card_num, flashcard in enumerate(list_flashcards, start=1):
-        definition_key, term_key = list(flashcard.keys())
-
-        print(f'Definition {card_num}: {flashcard[definition_key]}')
-        user_answer = input("\nWhat is the term? Or, enter 'STOP' to quit: ")
-
-        if user_answer == 'STOP':
-            return
-        print("The correct term was: ", flashcard[term_key], "\n")
-
-
 if __name__ == "__main__":
     user_id = get_user_id()
     do_again = True
@@ -258,11 +135,11 @@ if __name__ == "__main__":
         subject_id, subtopic_id, study_method_id = get_study_session_info()
 
         if study_method_id == 'Quiz':
-            run_quiz(subject_id, subtopic_id)
+            run_quiz(CLIENT, subject_id, subtopic_id)
         elif study_method_id == 'Flashcards':
-            run_flashcards(subject_id, subtopic_id)
+            run_flashcards(CLIENT, subject_id, subtopic_id)
         elif study_method_id == 'Explanation':
-            response = run_explanation(subject_id, subtopic_id)
+            response = run_explanation(CLIENT, subject_id, subtopic_id)
             print(response.choices[0].message.content.strip())
 
         while True:
