3f69b84c73ec838b2e36859694a4998ac760fd33
Chiagozie Okoye <c.i.okoye@wustl.edu>
2024-07-15 11:07:30 -0700

    Partially done

--- flashcards.py
+++ flashcards.py
@@ -1,12 +1,6 @@
 import json
 
 
-# # Create an OpenAPI client using the key.
-# MY_API_KEY = os.environ.get('OPENAI_KEY')
-# openai.api_key = MY_API_KEY
-# CLIENT = OpenAI(api_key=MY_API_KEY,)
-
-
 # This function creates 15 flashcards based on the subject and subtopic
 # provided by the user. The flashcards are created using the OpenAI API.
 # The flashcards are returned as a list of dictionaries.
@@ -26,15 +20,13 @@ def run_flashcards(CLIENT, subject: str, subtopic: str) -> list:
             ]
     }
     """
-
+    # The user_field is the prompt that is sent to the OpenAI API.
     user_field = (f"Package 15 flashcards about the subtopic {subtopic} "
                   f"about the subject {subject} and package them as a "
                   f"JSON file that follows the format {format_string}, "
                   f"and keep the name of the term out of the Definition "
                   f"field. Given a definition, I will be able to supply the "
                   f"vocabulary word.")
-
-    # print("\nStarting 15 Flashcards...\n")
 
     response = CLIENT.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -45,7 +37,9 @@ def run_flashcards(CLIENT, subject: str, subtopic: str) -> list:
             {"role": "user", "content": user_field}
         ]
     )
-
+    # The response is a JSON object that contains the flashcards. The list of
+    # flashcards is stored in a dictionary with the key being the term and
+    # the value being the definition.
     dictionary_flashcards = json.loads(response.choices[0].message.content)
     flashcard_key = list(dictionary_flashcards.keys())[0]
     list_flashcards = dictionary_flashcards[flashcard_key]
