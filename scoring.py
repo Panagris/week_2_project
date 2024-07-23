d4242b06d4d920b86eb745089be0b8bdaae9ba17
Anthony Rojas <anthony.rojas@utexas.edu>
2024-07-23 13:30:35 -0500

    Added Global Leaderboard and new XP Levels

--- scoring.py
+++ scoring.py
@@ -4,10 +4,11 @@ SCORE_TO_XP = [0, 0, 0, 100, 200, 300]
 
 
 def xp_level(xp):
-    LEVELS = ["Newbie", "Novice", "Apprentice", "Expert", "Master"]
+    LEVELS = ["Newbie", "Novice", "Apprentice", "Seasoned", "Expert", "Master",
+              "Sensei", "Grandmaster"]
 
-    # Cap XP at 400 to ensure the next line doesn't go out of bounds
-    xp = min(xp, 400)
+    # Cap XP to ensure the next line doesn't go out of bounds
+    xp = min(xp, (len(LEVELS) - 1) * 100)
 
     # Scoring system kept simple for the sake of demonstration
     # should be a little bit more involved in actuality
